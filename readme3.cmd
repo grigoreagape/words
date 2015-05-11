25ce3fd4f763704c5d64372e383f1253e11181e6
grigoreagape <grigoreagape@yahoo.com>
2015-05-11 14:02:54 +0200

    Create readme3.cmd

---
+++ readme3.cmd
@@ -0,0 +1,25 @@
+der Versuch, -(e)s, -e	s	încercare
+gleich	adv	asemănător, la fel
+gleichen	v	a fi la fel, a se asemăna
+antworten	v	a răspunde
+wer	pron	cine
+das Drehbuch	s	scenariu
+entschießen	v	a se decide
+seltsam	adj	straniu, ciudat
+draüber	prep	despre
+guken	v	a privi pe furiș
+anguken	v	a privi
+betreten	v	a intra, a păși în, a călca
+betreten	adj	rușinat
+die Schau, -, -en	s	expoziție
+ungewöhnlich	adj	neobisnuit, remarcabil
+neugierig	adj	curios
+herum	adv	împrejur, în preajma
+herumghehen	v	a se plimba împrejur
+das Glück, -(e)s,	s	noroc
+genauso	prep	la fel de
+erkennen	v	a discerne, a recunoaște
+erinnern	v	a-și audce aminte de ceva, a-și aminti
+widerfahren	v	 a se întâmpla
+schlimm	adj	rău, îngrozitor
+weil	prop	deoarece
